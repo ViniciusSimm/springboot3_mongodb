f639fdc18e31840427f0c457edf814fdbdd67e66
Vinicius Sylvestre <vinicius.sylvestre@hypeone.com>
2023-10-20 15:49:24 -0300

    connect to mongodb and test with findall

---
+++ src/main/java/com/mongodb/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.mongodb.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.domain.User;
+import com.mongodb.services.UserService;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+}
